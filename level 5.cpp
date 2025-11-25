1c39748e30d0cdd1f17e381b7a1944b08c751b92
User <User@ILAHE>
2025-11-25 23:43:57 +0400

    Add project files.

---
+++ level 5.cpp
@@ -0,0 +1,91 @@
+// level 5.cpp : This file contains the 'main' function. Program execution begins and ends there.
+//
+//level 1 TASK 3
+#include <iostream>
+using namespace std;
+void x(int a){
+	cout << a * a << endl;
+	cout << a * a * a << endl;
+  
+ }
+//level 1 TASK 2
+void x(float a, float b) {
+    float cem = a + b;
+    float hasil = a * b;
+    float ferq = a - b;
+    float bolme = a / b;
+    cout << cem << endl << hasil << endl << ferq << endl << bolme;
+}
+//level 1 task 6
+float edediort(float a, float b, float c) {
+    float x = (a + b + c) / 3;
+    cout << x << endl;
+}
+
+/* level 1 task 9  */  
+void saat(float x) {
+    int deq = a / 60;
+    int saat = a / 3600;
+    cout << deqiqe << endl << saat;
+}
+int main() {
+    saat(3600);
+}
+//level 2 task 1
+void boyuktap(int a, int b) {
+    if (a > b)
+        cout << a << endl;
+    else if (b > a)
+        cout << b << endl;
+    else
+        cout << "beraber" << endl;
+}
+//level 2 task 
+void yoxla(int a) {
+    if (a > 0) {
+        cout << "musbet" << endl;
+    }
+    else if (a < 0) {
+        cout << "menfi" << endl;
+    }
+    else {
+        cout << "sifir" << endl;
+    }
+ }
+//level 3 task 1
+void ededler() {
+    for (int i = 1; i <= 10; i++) {
+        cout << i;
+    }
+    cout << endl;
+}
+//level 3 task 2
+void cem() {
+    int s = 0;
+    for (int i = 1; i <= 100; i++) {
+        s += 1;
+    }
+    cout << "cem" << s << endl;
+}
+//level 1 task 8
+void kvadrat(int a) {
+    int sahe = a * a;
+    int perimetr = 4 * a;
+    cout << sahe << endl;
+    cout << perimetr << endl;
+//}
+//level 1 task 4
+    void cevre(float r){
+        float PI = 3.14;
+        float sahe = r * r * PI;
+        float uzunluq = 2 * PI * r;
+        cout << sahe << endl << uzunluq << endl;
+
+    
+}
+       
+    
+
+  
+
+
